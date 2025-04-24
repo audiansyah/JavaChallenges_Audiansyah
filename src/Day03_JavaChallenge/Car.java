2245d993f5d9eb575c18104dfd7f9633a570501c
Audiansyah <msaudiansyah@gmail.com>
2025-04-24 22:31:27 +0700

    Java Challenge Day 3 OOP

---
+++ src/Day03_JavaChallenge/Car.java
@@ -0,0 +1,12 @@
+package Day03_JavaChallenge;
+
+import java.time.LocalDate;
+
+public class Car extends VehiclePurchase {
+    public Car(String noPolice, VehicleType vehicletype, int year, double price, double tax, int seat,
+               LocalDate transactionDate) {
+        super(noPolice, vehicletype, year, price, tax, seat, transactionDate);
+        totalCar++;
+    }
+
+}
